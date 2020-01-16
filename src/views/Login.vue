2eaed553247e30e4ac2c91eed61447cf69f70755
Alugbin Abiodun Olutola <alugbin.abiodun@gmail.com>
2020-01-16 13:59:20 +0100

    All set for vuetify

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login_page">
+        <md-content>
+          <div class="title">
+            Testing MIC
+          </div>
+        </md-content>
+    </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  components: {
+  },
+};
+
+</script>
