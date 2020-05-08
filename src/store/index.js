4e4072eba074e9de76c15d04533c90af98877a70
olawalle <it_unit@cardinalstone.com>
2020-05-09 00:45:52 +0100

    stash

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 /** @format */
 
-import Vue from 'vue'
-import Vuex from 'vuex'
-import VuexPersistence from 'vuex-persist'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const vuexLocal = new VuexPersistence({
   storage: window.localStorage,
-})
+});
 
 export default new Vuex.Store({
   state: {
@@ -24,65 +24,39 @@ export default new Vuex.Store({
     feedback: {},
   },
   getters: {
-    getCBTInfo: (state) => state.info,
-    responses: (state) => state.responses,
-    getToken: (state) => state.token,
-    getUser: (state) => state.user,
-    getQuestions: (state) => state.questions,
-    getCurrentTest: (state) => state.testID,
-    getFeedback: (state) => state.feedback,
+    getCBTInfo: state => state.info,
+    responses: state => state.responses,
+    getToken: state => state.token,
+    getUser: state => state.user,
+    getQuestions: state => state.questions,
+    getCurrentTest: state => state.testID,
+    getFeedback: state => state.feedback,
   },
   mutations: {
     setCBTInfo(state, info) {
-      state.info = info
+      state.info = info;
     },
     updateResponse(state, response) {
-      state.responses.set(response.questionID, response.response)
+      state.responses.set(response.questionID, response.response);
     },
     setUserToken(state, token) {
-      state.token = token
+      state.token = token;
     },
     setUser(state, user) {
-      state.user = user
+      state.user = user;
     },
     setTestID(state, testID) {
-      state.testID = testID
+      state.testID = testID;
     },
     setQuestions(state, questions) {
-      state.questions = questions
+      state.questions = questions;
     },
     setFeedback(state, feedback) {
-      state.feedback = feedback
+      state.feedback = feedback;
     },
   },
   actions: {
     setCBTInfo(context, payload) {
-<<<<<<< HEAD
-      context.commit('setCBTInfo', payload)
-    },
-    updateResponse(context, payload) {
-      context.commit('updateResponse', payload)
-    },
-    updateUser(context, payload) {
-      context.commit('setUser', payload)
-    },
-    updateToken(context, payload) {
-      context.commit('setUserToken', payload)
-    },
-    updateTestID(context, payload) {
-      context.commit('setTestID', payload)
-    },
-    updateTestQuestions(context, payload) {
-      context.commit('setQuestions', payload)
-    },
-    updateFeedback(context, payload) {
-      context.commit('setFeedback', payload)
-    },
-  },
-  modules: {},
-  plugins: [vuexLocal.plugin],
-})
-=======
       context.commit('setCBTInfo', payload);
     },
     updateResponse(context, payload) {
@@ -105,5 +79,5 @@ export default new Vuex.Store({
     },
   },
   modules: {},
+  plugins: [vuexLocal.plugin],
 });
->>>>>>> 48099d6e89b056c812788c2f7d58f7b2a79c588a
