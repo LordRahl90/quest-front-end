4de6ad7dabb0250196f14ef79589933d5ed7a1bf
Alugbin Abiodun Olutola <alugbin.abiodun@gmail.com>
2020-07-04 01:18:05 +0200

    changed IP to domain name

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /** @format */
 
-export const BASE_URL = "http://159.89.55.133:8000/api";
-export const BACKEND = "http://159.89.55.133:8000/api";
+export const BASE_URL = "http://quest-tutors.com:8000/api";
+export const BACKEND = "http://quest-tutors.com:8000/api";
 // export const BACKEND = "http://localhost:8000/api";
 // export const BASE_URL = "http://localhost:8000/api";
