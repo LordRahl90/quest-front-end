9f71dc9a38dbce4efc436326de1d95095092c0c4
Alugbin Abiodun Olutola <alugbin.abiodun@gmail.com>
2020-12-08 01:46:53 +0100

    updated the backend link

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 /** @format */
 export const BASE_URL = 'http://quest-tutors.com:8000/api';
-export const BACKEND = 'http://quest-tutors.com:8000/api';
+export const BACKEND = 'http://admin.quest-tutors.com/api';
