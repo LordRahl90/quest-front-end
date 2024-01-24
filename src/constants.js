616401ab2773d54e4c494882b4683b7d87bc8c6a
Alugbin Abiodun Olutola <alugbin.abiodun@gmail.com>
2024-01-24 20:10:10 +0100

    updated

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /** @format */
 export const BASE_URL = 'http://student.quest-tutors.com';
 // export const BASE_URL = 'http://localhost:8802';
-export const BACKEND = 'http://admin.quest-tutors.com/api';
-// export const BACKEND = 'http://localhost:8000/api';
+// export const BACKEND = 'http://admin.quest-tutors.com/api';
+export const BACKEND = 'http://localhost:8000/api';
